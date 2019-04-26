dd22bc52bfdf47086a13e82c6f5d5b8488269db9
Marcos Schratzenstaller <marksabbath@gmail.com>
2019-04-26 18:49:43 -0300

    Rolling back plugin header to simple-menu.php

--- genesis-simple-menus.php
+++ genesis-simple-menus.php
@@ -1,16 +1,6 @@
 <?php
 /**
- * Plugin Name: Genesis Simple Menus
- * Plugin URI: https://github.com/copyblogger/genesis-simple-menus
- * Description: Genesis Simple Menus allows you to select a WordPress menu for secondary navigation on individual posts/pages.
- * Version: 1.0.1
- * Author: StudioPress
- * Author URI: http://www.studiopress.com/
- * License: GNU General Public License v2.0 (or later)
- * License URI: https://www.opensource.org/licenses/gpl-license.php
- *
- * Text Domain: genesis-simple-menus
- * Domain Path: /languages
+ * Genesis Simple Menus file.
  *
  * @package genesis-simple-menu
  */
--- simple-menu.php
+++ simple-menu.php
@@ -1,6 +1,16 @@
 <?php
 /**
- * Import the main file.
+ * Plugin Name: Genesis Simple Menus
+ * Plugin URI: https://github.com/copyblogger/genesis-simple-menus
+ * Description: Genesis Simple Menus allows you to select a WordPress menu for secondary navigation on individual posts/pages.
+ * Version: 1.0.1
+ * Author: StudioPress
+ * Author URI: http://www.studiopress.com/
+ * License: GNU General Public License v2.0 (or later)
+ * License URI: https://www.opensource.org/licenses/gpl-license.php
+ *
+ * Text Domain: genesis-simple-menus
+ * Domain Path: /languages
  *
  * @package genesis-simple-menu
  */
