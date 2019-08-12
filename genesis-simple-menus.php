8f40be92c656c8a6be9a463462f516fd78c6a2e0
Marcos Schratzenstaller <marksabbath@gmail.com>
2019-08-12 13:48:40 -0300

    Preparing for release 1.1.0

--- genesis-simple-menus.php
+++ genesis-simple-menus.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 define( 'GENESIS_SIMPLE_MENU_SETTINGS_FIELD', 'genesis_simple_menu_settings' );
-define( 'GENESIS_SIMPLE_MENU_VERSION', '1.0.1' );
+define( 'GENESIS_SIMPLE_MENU_VERSION', '1.1.0' );
 define( 'GENESIS_SIMPLE_MENU_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'GENESIS_SIMPLE_MENU_PLUGIN_URL', plugins_url( '', __FILE__ ) );
 
