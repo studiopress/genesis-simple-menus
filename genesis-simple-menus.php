344136c411dfba1904f2cf224b8ea001c5259b84
Nick Cernis <nick@cern.is>
2024-10-15 09:33:45 +0200

    [LOC-6169] Fetch plugin updates from WPE servers (#29)
    * feat: get plugin updates from WPE
    
    Adapted from the sample code at https://github.com/wpengine/plugin-updater.
    
    * ci: comment out wp-svn usage
    
    This plugin can no longer be published on WP.org due to blocked access.
    
    We'll just lint in Circle for now on the non-master branch.
    
    * chore: fix plugin_basename for updater
    
    This plugin has a different entry point.
    
    * chore: add npm run zip script
    
    To make it easier to package a zip for production.
    
    * chore: bump version to 1.1.3, add changelog
    
    * chore: ignore phpcs warning for dirname

--- genesis-simple-menus.php
+++ genesis-simple-menus.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 define( 'GENESIS_SIMPLE_MENU_SETTINGS_FIELD', 'genesis_simple_menu_settings' );
-define( 'GENESIS_SIMPLE_MENU_VERSION', '1.1.2' );
+define( 'GENESIS_SIMPLE_MENU_VERSION', '1.1.3' );
 define( 'GENESIS_SIMPLE_MENU_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'GENESIS_SIMPLE_MENU_PLUGIN_URL', plugins_url( '', __FILE__ ) );
 
@@ -42,3 +42,18 @@ function genesis_simple_menus() {
  * Initialize the object on `plugins_loaded`.
  */
 add_action( 'plugins_loaded', array( Genesis_Simple_Menus(), 'init' ) );
+
+/**
+ * Initialize checking of plugin updates from WP Engine.
+ */
+function genesis_simple_menus_check_for_upgrades() {
+	$properties = array(
+		'plugin_slug'     => 'genesis-simple-menus',
+		// phpcs:ignore
+		'plugin_basename' => plugin_basename( dirname( __FILE__ ) . '/simple-menu.php' ),
+	);
+
+	require_once __DIR__ . '/includes/class-genesis-simple-menus-plugin-updater.php';
+	new Genesis_Simple_Menus_Plugin_Updater( $properties );
+}
+add_action( 'admin_init', 'genesis_simple_menus_check_for_upgrades' );
