bff4adc1e0b0dcd05c6b4d6cca854b3a9bf16107
Nick Cernis <nick@cern.is>
2025-04-09 17:34:19 +0200

    Defer init until menus are registered
    Genesis 3.6 moved menu registration logic to after_setup_theme, so we
    need to wait until then before running any additional menu logic.

--- includes/class-genesis-simple-menus.php
+++ includes/class-genesis-simple-menus.php
@@ -76,7 +76,7 @@ final class Genesis_Simple_Menus {
 		/**
 		 * Include and Instantiate.
 		 */
-		add_action( 'genesis_setup', array( $this, 'instantiate' ) );
+		add_action( 'after_setup_theme', array( $this, 'instantiate' ), 11 );
 	}
 
 
